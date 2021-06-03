9bc2772871e931f02f838a50ff44fa79f0d8ab85
Vlad Burca <tikiguy1642@gmail.com>
2021-06-03 21:36:52 +0300

    web analyzer can get the gallery link if exists

--- webpage_analyzer/index.py
+++ webpage_analyzer/index.py
@@ -1,5 +1,23 @@
+import json
 import requests as req
+from bs4 import BeautifulSoup
 
-resp = req.get("https://www.imagebam.com/view/GAKC")
-# print(resp.text)
-print(resp.status_code)
+URL = "https://www.imagebam.com/view/MEMYAQ"
+resp = req.get(URL)
+status_req = resp.status_code
+if status_req == 200:
+
+    soup = BeautifulSoup(resp.content, 'html.parser')
+    soup = soup.body.div.main
+    soup = soup.find_all(attrs={"class": "fas fa-ellipsis-h"})
+    # soup = soup.find_previous("a")["href"]
+    # soup = str(soup)
+    if len(soup) > 0:
+        soup = soup[0]
+        soup = soup.find_previous("a")["href"]
+
+        print("Picture belongs to a Gallery {}".format(soup))
+    else:
+        print("Picture does not belong to a gallery, URL will be stored in Links database {}".format(URL))
+else:
+    print("URL request STATUS is {}".format(status_req))
