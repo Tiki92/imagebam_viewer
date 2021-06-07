93faf9fb01038c0c567dc28da08a580e5dcac43a
Vlad Burca <tikiguy1642@gmail.com>
2021-06-07 21:52:00 +0300

    removed thumbnails

--- viewer/viewer/settings.py
+++ viewer/viewer/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'display',
     'django_crontab',
-    'thumbnails',
 ]
 
 MIDDLEWARE = [
@@ -132,6 +131,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-THUMBNAIL_PATH = "viewer/display/thumb"
